513d37468aa59d41a712d4bc5227a2f8c32adbdc
haikal <pchaikal54@gmail.com>
2026-02-06 21:28:56 +0700

    tugas laravel template seodash

--- inventoryapp_haikal/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\FormController;
-
-Route::get('/', [DashboardController::class, 'home'])->name('home');
-Route::get('/register', [FormController::class, 'register'])->name('register');
-Route::post('/welcome', [FormController::class, 'welcome'])->name('welcome');
